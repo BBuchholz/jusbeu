b553136b65264200da398d2c07e837111b823210
Brent Buchholz <brentbuchholz@gmail.com>
2023-08-20 16:52:10 -0500

    Add meditation

---
+++ src/pages/myrKiSs/MEDS.vue
@@ -0,0 +1,6 @@
+<template>
+  <p>SELECT A MEDITATION</p>
+  <RouterLink to="/myrKiSs/REALHEAL" replace>
+    <img class="card" src="/REALHEAL-73a4.png">
+  </RouterLink>
+</template>
---
+++ src/pages/myrKiSs/REALHEAL.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p><a href="https://play.google.com/store/apps/details?id=com.mymaloka.companion">Launch Maloka App</a></p>
+    <!-- <a href="https://anxious-grove.netlify.app/" target="_blank"> -->
+    <img class="card" src="/REALHEAL-73a4.png">
+    <!-- </a> -->
+  </div>
+</template>
+
+<style>
+  .card {
+    max-width: 50%;
+    height: auto;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    border: 5px;
+    border-color: white;
+    border-style: solid;
+    border-radius: 1%;
+  }
+</style>
