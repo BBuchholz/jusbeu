403460efd7d56a40948013c7f2ad1b73837a8fa3
Brent Buchholz <brentbuchholz@gmail.com>
2023-10-07 04:46:31 -0500

    Add COVENTRY

---
+++ src/pages/myrKiSs/COVENTREE.vue
@@ -0,0 +1,33 @@
+<template>
+  <p class="display">
+    COMING SOON: WELLcome 2 tha GarDin
+  </p>
+  <div>
+    <!-- <a href="#" target="_blank"> -->
+    <img class="card" src="https://madamadam.s3.us-east-2.amazonaws.com/CGBA-f6df_CARD.png">
+    <!-- </a> -->
+  </div>
+  <p class="display">
+    card attribution info here
+  </p>
+</template>
+
+  <style>
+    .display {
+      margin: 40px;
+    }
+
+    .card {
+      max-width: 50%;
+      height: auto;
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: 50px;
+      margin-bottom: 50px;
+      border: 5px;
+      border-color: white;
+      border-style: solid;
+      border-radius: 1%;
+    }
+  </style>
---
+++ src/pages/myrKiSs/COVENTRY.vue
@@ -0,0 +1,33 @@
+<template>
+  <p class="display">
+    Coventry Grove is Invite Only. If you landed here intentionally, you may want to Double ChecK with WhomEver sent You...
+  </p>
+  <div>
+    <!-- <a href="#" target="_blank"> -->
+    <img class="card" src="https://madamadam.s3.us-east-2.amazonaws.com/CGBA-f6df_CARD.png">
+    <!-- </a> -->
+  </div>
+  <p class="display">
+    card attribution info here
+  </p>
+</template>
+
+  <style>
+    .display {
+      margin: 40px;
+    }
+
+    .card {
+      max-width: 50%;
+      height: auto;
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: 50px;
+      margin-bottom: 50px;
+      border: 5px;
+      border-color: white;
+      border-style: solid;
+      border-radius: 1%;
+    }
+  </style>
