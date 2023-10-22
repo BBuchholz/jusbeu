681306bbbce6a26c00846bbd79f6bd50b1748a17
Brent Buchholz <brentbuchholz@gmail.com>
2023-10-22 05:03:32 -0500

    Add Fire Tests

---
+++ test/ToM_Minor_Fire.test.js
@@ -0,0 +1,134 @@
+import {
+  expect,
+  test,
+} from 'vitest'
+
+import {
+  getPotentialMatchesFor,
+} from '../src/pages/allTarotMarseilles'
+
+test('Ace of Wands', () => {
+  expect(getPotentialMatchesFor('Ace of Wands'))
+    .toContain('Wands')
+
+  // By Modality
+  // By Element
+  // By Sign
+})
+
+test('Two of Wands', () => {
+  expect(getPotentialMatchesFor('Two of Wands'))
+    .toContain('Wands')
+
+  // By Modality
+  // By Element
+  // By Sign
+})
+
+test('Three of Wands', () => {
+  expect(getPotentialMatchesFor('Three of Wands'))
+    .toContain('Wands')
+
+  // By Modality
+  // By Element
+  // By Sign
+})
+
+test('Four of Wands', () => {
+  expect(getPotentialMatchesFor('Four of Wands'))
+    .toContain('Wands')
+
+  // By Modality
+  // By Element
+  // By Sign
+})
+
+test('Five of Wands', () => {
+  expect(getPotentialMatchesFor('Five of Wands'))
+    .toContain('Wands')
+
+  // By Modality
+  // By Element
+  // By Sign
+})
+
+test('Six of Wands', () => {
+  expect(getPotentialMatchesFor('Six of Wands'))
+    .toContain('Wands')
+
+  // By Modality
+  // By Element
+  // By Sign
+})
+
+test('Seven of Wands', () => {
+  expect(getPotentialMatchesFor('Seven of Wands'))
+    .toContain('Wands')
+
+  // By Modality
+  // By Element
+  // By Sign
+})
+
+test('Eight of Wands', () => {
+  expect(getPotentialMatchesFor('Eight of Wands'))
+    .toContain('Wands')
+
+  // By Modality
+  // By Element
+  // By Sign
+})
+
+test('Nine of Wands', () => {
+  expect(getPotentialMatchesFor('Nine of Wands'))
+    .toContain('Wands')
+
+  // By Modality
+  // By Element
+  // By Sign
+})
+
+test('Ten of Wands', () => {
+  expect(getPotentialMatchesFor('Ten of Wands'))
+    .toContain('Wands')
+
+  // By Modality
+  // By Element
+  // By Sign
+})
+
+test('Page of Wands', () => {
+  expect(getPotentialMatchesFor('Page of Wands'))
+    .toContain('Wands')
+
+  // By Modality
+  // By Element
+  // By Sign
+})
+
+test('Knight of Wands', () => {
+  expect(getPotentialMatchesFor('Knight of Wands'))
+    .toContain('Wands')
+
+  // By Modality
+  // By Element
+  // By Sign
+})
+
+test('Queen of Wands', () => {
+  expect(getPotentialMatchesFor('Queen of Wands'))
+    .toContain('Wands')
+
+  // By Modality
+  // By Element
+  // By Sign
+})
+
+test('King of Wands', () => {
+  expect(getPotentialMatchesFor('King of Wands'))
+    .toContain('Wands')
+
+  // By Modality
+  // By Element
+  // By Sign
+})
