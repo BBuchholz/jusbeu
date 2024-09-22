bebd8a37f890d2dfc0c4f6f9a6cb4cc9d4b1efb7
Brent Buchholz <brentbuchholz@gmail.com>
2024-09-21 19:52:29 -0500

    Fix potential hydration issues according to compiler warning

--- src/pages/seeds/[anyMyrKi].vue
+++ src/pages/seeds/[anyMyrKi].vue
@@ -85,14 +85,14 @@ if (foundSeed
     <div v-if="user.otherMyrKis.length">
       <p mt-4 text-sm>
         <span opacity-75>Recently attempts:</span>
-        <ul>
-          <li v-for="otherMyrKi in user.otherMyrKis" :key="otherMyrKi">
-            <RouterLink :to="`/myrKiSs/${otherMyrKi}`" replace>
-              {{ otherMyrKi }}
-            </RouterLink>
-          </li>
-        </ul>
       </p>
+      <ul>
+        <li v-for="otherMyrKi in user.otherMyrKis" :key="otherMyrKi">
+          <RouterLink :to="`/myrKiSs/${otherMyrKi}`" replace>
+            {{ otherMyrKi }}
+          </RouterLink>
+        </li>
+      </ul>
     </div>
 
     <div>
